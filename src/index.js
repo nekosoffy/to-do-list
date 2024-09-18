8ac3987a09ed18a9b5d74ca7f526b48592887785
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-18 19:56:58 -0300

    feat: create function to generate to-do lists

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import "./reset.css";
 import "./styles.css";
+
+const projectList = [{ default: [] }];
+let activeProject = 0;
+
+function createToDoList(title, description, dueDate, priority, notes) {
+    const list = { title, description, dueDate, priority, notes };
+    const i = activeProject;
+    const project = projectList[i];
+    
+    for (const key in project) {
+        project[key].push(list);
+    }
+}

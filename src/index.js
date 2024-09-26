2839ffc1d9dce1eb3e7d4b847483eafac5339f16
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-26 16:02:25 -0300

    refactor: remove unnecessary parts of the code

--- src/index.js
+++ src/index.js
@@ -4,277 +4,241 @@ import { projectDisplay } from "./project-display.js";
 import { taskDisplay } from "./task-display.js";
 
 const manageProject = () => {
-    let projectList = [ 
-        { 
-            "Default Project": [],
-            latestDueDate: null,
-            id: 0
+  let projectList = [
+    {
+      "Default Project": [],
+      latestDueDate: null,
+      id: 0,
+    },
+  ];
+
+  let selectedProject = 0;
+
+  function create(title) {
+    const project = {
+      [title]: [],
+      latestDueDate: null,
+      id: projectList.length,
+    };
+
+    projectList.push(project);
+    sortProjectsByLatestDueDate();
+  }
+
+  function edit(projectIndex, newTitle) {
+    const currentTasks = Object.values(projectList[projectIndex])[0];
+    const editedProject = { [newTitle]: currentTasks };
+    projectList[projectIndex] = editedProject;
+
+    sortProjectsByLatestDueDate();
+  }
+
+  function remove(projectIndex) {
+    projectList.splice(projectIndex, 1);
+
+    if (selectedProject >= projectList.length) {
+      selectedProject = projectList.length - 1;
+    }
+
+    if (!projectList.length) {
+      // To always make sure there's the default project.
+      projectList.push({
+        "Default Project": [],
+        latestDueDate: null,
+        id: 0,
+      });
+      selectedProject = 0;
+    }
+
+    sortProjectsByLatestDueDate();
+  }
+
+  function select(projectIndex) {
+    selectedProject = projectIndex;
+  }
+
+  function updateLatestDueDate(projectIndex) {
+    const tasks = Object.values(projectList[projectIndex])[0];
+    let latestDate = null;
+
+    tasks.forEach((task) => {
+      if (task[2]["Due Date:"]) {
+        const dueDate = task[2]["Due Date:"];
+        if (!latestDate || dueDate < latestDate) {
+          latestDate = dueDate;
         }
-    ];
+      }
+    });
 
-    let selectedProject = 0;
+    projectList[projectIndex].latestDueDate = latestDate;
+  }
 
-    function create(title) {
-        const project = { 
-            [title]: [],
-            latestDueDate: null,
-            id: projectList.length
-        };
+  function sortProjectsByLatestDueDate() {
+    const previousSelectedId = projectList[selectedProject].id;
 
-        console.log(projectList.length);
+    projectList.sort((a, b) => {
+      const dateA = a.latestDueDate;
+      const dateB = b.latestDueDate;
 
-        projectList.push(project);
+      if (dateA === null && dateB === null) {
+        return 0;
+      } else if (dateA === null) {
+        return -1;
+      } else if (dateB === null) {
+        return 1;
+      }
 
-        console.log(projectList.length);
+      return new Date(dateB) - new Date(dateA);
+    });
 
-        console.log(projectList);
+    const newSelectedIndex = projectList.findIndex(
+      (project) => project.id === previousSelectedId
+    );
+    select(newSelectedIndex);
+  }
 
-        sortProjectsByLatestDueDate();
+  function populateStorage() {
+    localStorage.setItem("projects", JSON.stringify(projectList));
+  }
 
-        console.log(projectList);
+  function getStorage() {
+    if (!localStorage.getItem("projects")) {
+      populateStorage();
+    } else {
+      projectList = JSON.parse(localStorage.getItem("projects"));
     }
+  }
 
-    function edit(projectIndex, newTitle) {
-        if (projectIndex >= projectList.length || 
-            projectIndex < 0 ||
-            !Number.isInteger(projectIndex)) {
-                console.log("Invalid value!");
-                return;
-        }
+  const getProjectList = () => projectList;
+  const getSelectedProject = () => selectedProject;
 
-        const currentTasks = Object.values(projectList[projectIndex])[0];
-        const editedProject = {[newTitle] : currentTasks};
-        projectList[projectIndex] = editedProject;
-
-        sortProjectsByLatestDueDate();
-    }
-
-    function remove(projectIndex) {
-        if (projectIndex >= projectList.length || 
-            projectIndex < 0 ||
-            !Number.isInteger(projectIndex)) {
-                console.log("Invalid value!");
-                return;
-        }
-
-        projectList.splice(projectIndex, 1);
-
-        if (selectedProject >= projectList.length) {
-            selectedProject = projectList.length - 1;
-        }
-
-        if (!projectList.length) { // To always make sure there's the default project.
-            projectList.push( 
-                { 
-                    "Default Project": [],
-                    latestDueDate: null,
-                    id: 0
-                }
-            ); 
-            selectedProject = 0;
-        }
-
-        sortProjectsByLatestDueDate();
-        console.log(getSelectedProject());
-        console.log(getProjectList());
-    }
-
-    function select(projectIndex) {
-        if (projectIndex >= projectList.length || 
-            projectIndex < 0 ||
-            !Number.isInteger(projectIndex)) {
-                console.log("Invalid value!");
-                return;
-        }
-
-        selectedProject = projectIndex;
-    }
-
-    function updateLatestDueDate(projectIndex) {
-        const tasks = Object.values(projectList[projectIndex])[0];
-        let latestDate = null;
-    
-        tasks.forEach(task => {
-            if (task[2]["Due Date:"]) {
-                const dueDate = task[2]["Due Date:"];
-                if (!latestDate || dueDate < latestDate) {
-                    latestDate = dueDate;
-                }
-            }
-        });
-    
-        projectList[projectIndex].latestDueDate = latestDate;
-    }
-
-    function sortProjectsByLatestDueDate() {
-        const previousSelectedId = projectList[selectedProject].id;
-        
-        projectList.sort((a, b) => {
-            const dateA = a.latestDueDate;
-            const dateB = b.latestDueDate;
-    
-            if (dateA === null && dateB === null) {
-                
-                return 0;
-            } else if (dateA === null) {
-                return -1;
-            } else if (dateB === null) {
-                return 1;
-            }
-    
-            return new Date(dateB) - new Date(dateA);
-            
-        });
-
-        const newSelectedIndex = projectList.findIndex(project => project.id === previousSelectedId);
-        select(newSelectedIndex);
-    }
-
-    function populateStorage() {
-        localStorage.setItem("projects", JSON.stringify(projectList));
-    }
-
-    function getStorage() {
-        if (!localStorage.getItem("projects")) {
-            populateStorage();
-        } else {
-            projectList = JSON.parse(localStorage.getItem("projects"));
-        }   
-    }
-
-    const getProjectList = () => projectList;
-    const getSelectedProject = () => selectedProject;
-
-    return { create, edit, remove, select, getSelectedProject, getProjectList, 
-    populateStorage, getStorage, updateLatestDueDate, sortProjectsByLatestDueDate };
-}
+  return {
+    create,
+    edit,
+    remove,
+    select,
+    getSelectedProject,
+    getProjectList,
+    populateStorage,
+    getStorage,
+    updateLatestDueDate,
+    sortProjectsByLatestDueDate,
+  };
+};
 
 const project = manageProject();
 
 const manageTask = () => {
-    const getTasksArray = () => {
-        const currentProject = project.getProjectList()[project.getSelectedProject()];
-        return Object.values(currentProject)[0];
-    };
-    
-    function create(title, description, dueDate, priority, notes, ...checklist) {
-        const tasksArray = getTasksArray();
+  const getTasksArray = () => {
+    const currentProject =
+      project.getProjectList()[project.getSelectedProject()];
+    return Object.values(currentProject)[0];
+  };
 
-        const formattedChecklist = checklist.map(item => {
-            return {[item]: "not completed"};
-        });
+  function create(title, description, dueDate, priority, notes, ...checklist) {
+    const tasksArray = getTasksArray();
 
-        const date = dueDate ? new Date(dueDate + 'T00:00:00') : null;
+    const formattedChecklist = checklist.map((item) => {
+      return { [item]: "not completed" };
+    });
 
-        const list = [
-            { "Title:": title },
-            { "Description:": description },
-            { "Due Date:": date },
-            { "Priority:": priority },
-            { "Notes:": notes },
-            { "Checklist:": checklist },
-            { "Completed:": false }
-        ];
+    const date = dueDate ? new Date(dueDate + "T00:00:00") : null;
 
-        tasksArray.push(list);
-        sortTasksByDueDate();
-        project.updateLatestDueDate(project.getSelectedProject());
-        project.sortProjectsByLatestDueDate();
+    const list = [
+      { "Title:": title },
+      { "Description:": description },
+      { "Due Date:": date },
+      { "Priority:": priority },
+      { "Notes:": notes },
+      { "Checklist:": checklist },
+      { "Completed:": false },
+    ];
+
+    tasksArray.push(list);
+    sortTasksByDueDate();
+    project.updateLatestDueDate(project.getSelectedProject());
+    project.sortProjectsByLatestDueDate();
+  }
+
+  function edit(taskIndex, property, newValue) {
+    const tasksArray = getTasksArray();
+
+    let validProperties = [
+      "title",
+      "description",
+      "dueDate",
+      "priority",
+      "notes",
+    ];
+
+    if (validProperties.includes(property)) {
+      tasksArray[taskIndex][0][property] = newValue;
     }
 
-    function edit(taskIndex, property, newValue) {
-        const tasksArray = getTasksArray();
+    sortTasksByDueDate();
+    project.updateLatestDueDate(project.getSelectedProject());
+    project.sortProjectsByLatestDueDate();
+  }
 
-        if (taskIndex >= tasksArray.length || 
-            taskIndex < 0 ||
-            !Number.isInteger(taskIndex)) {
-                console.log("Invalid value!");
-                return;
-        }
+  function remove(taskIndex) {
+    const tasksArray = getTasksArray();
 
-        let validProperties = [
-            "title", "description", "dueDate",
-            "priority", "notes"
-        ] 
+    tasksArray.splice(taskIndex, 1);
+    sortTasksByDueDate();
+    project.updateLatestDueDate(project.getSelectedProject());
+    project.sortProjectsByLatestDueDate();
+  }
 
-        if (validProperties.includes(property)) {
-            tasksArray[taskIndex][0][property] = newValue;
-        }
+  function editChecklist(taskIndex, checklistItemIndex, newItem) {
+    const tasksArray = getTasksArray();
 
-        sortTasksByDueDate();
-        project.updateLatestDueDate(project.getSelectedProject());
-        project.sortProjectsByLatestDueDate();
-    }
+    const currentCompletion = Object.values(
+      tasksArray[taskIndex]["checklist"][checklistItemIndex]
+    )[0];
+    const editedChecklistItem = { [newItem]: currentCompletion };
+    tasksArray[taskIndex]["checklist"][checklistItemIndex] =
+      editedChecklistItem;
+  }
 
-    function remove(taskIndex) {
-        const tasksArray = getTasksArray();
+  function markCompletion(taskIndex) {
+    const tasksArray = getTasksArray();
 
-        if (taskIndex >= tasksArray.length || 
-            taskIndex < 0 ||
-            !Number.isInteger(taskIndex)) {
-                console.log("Invalid value!");
-                return;
-        }
+    tasksArray[taskIndex]["completed"] = !tasksArray[taskIndex]["completed"];
+  }
 
-        tasksArray.splice(taskIndex, 1);
-        sortTasksByDueDate();
-        project.updateLatestDueDate(project.getSelectedProject());
-        project.sortProjectsByLatestDueDate();
-    }
+  function sortTasksByDueDate() {
+    const tasksArray = getTasksArray();
 
-    function editChecklist(taskIndex, checklistItemIndex, newItem) {
-        const tasksArray = getTasksArray();
-        
-        if (taskIndex >= tasksArray.length || 
-            taskIndex < 0 ||
-            !Number.isInteger(taskIndex)) {
-                console.log("Invalid value!");
-                return;
-        }
+    tasksArray.sort((a, b) => {
+      const dateA = a[2]["Due Date:"];
+      const dateB = b[2]["Due Date:"];
 
-        const currentCompletion = Object.values(tasksArray[taskIndex]["checklist"][checklistItemIndex])[0];
-        const editedChecklistItem = {[newItem] : currentCompletion};
-        tasksArray[taskIndex]["checklist"][checklistItemIndex] = editedChecklistItem;
-    }
+      if (dateA && dateB) {
+        return dateA - dateB;
+      }
 
-    function markCompletion(taskIndex) {
-        const tasksArray = getTasksArray();
+      if (dateA) {
+        return -1;
+      }
 
-        if (taskIndex >= tasksArray.length || 
-            taskIndex < 0 ||
-            !Number.isInteger(taskIndex)) {
-                console.log("Invalid value!");
-                return;
-        }
+      if (dateB) {
+        return 1;
+      }
 
-        tasksArray[taskIndex]["completed"] = !tasksArray[taskIndex]["completed"];
-    }
+      return 0;
+    });
+  }
 
-    function sortTasksByDueDate() {
-        const tasksArray = getTasksArray();
-
-        tasksArray.sort((a, b) => {
-            const dateA = a[2]["Due Date:"];
-            const dateB = b[2]["Due Date:"];
-
-            if (dateA && dateB) {
-                return dateA - dateB;
-            }
-
-            if (dateA) {
-                return -1;
-            }
-
-            if (dateB) {
-                return 1;
-            }
-
-            return 0;
-        });
-    }
-
-    return { remove, create, edit, editChecklist, markCompletion, getTasksArray, sortTasksByDueDate };
-}
+  return {
+    remove,
+    create,
+    edit,
+    editChecklist,
+    markCompletion,
+    getTasksArray,
+    sortTasksByDueDate,
+  };
+};
 
 const task = manageTask();
 

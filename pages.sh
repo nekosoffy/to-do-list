332ab064960c8a799d2466f6c93de190374064f4
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-11-16 23:49:48 -0300

    build: adjust files for page deploy

---
+++ pages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git commit -am "Save uncommited changes (WIP)"
+git branch --delete --force gh-pages
+git checkout --orphan gh-pages
+git add -f dist
+git commit -m "build: update GitHub pages"
+git push -f origin gh-pages && git checkout -
+git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -
